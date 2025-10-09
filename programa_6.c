d4489c669c1c17de982339311a0f40691c895e78
DART1511 <ramirez.t.david510@gmail.com>
2025-10-09 14:33:12 -0600

    Update programa_6.c

--- programa_6.c
+++ programa_6.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    int op;
+int main(void)
+{
+    int op = 0;
 
     printf("\tMenú\n\n");
     printf("Elegir la opción deseada\n");
-    printf("1) Ingresar\n");
-    printf("2) Registrarse\n");
-    printf("3) Salir\n");
+    printf("(1) Ingresar\n");
+    printf("(2) Registrarse\n");
+    printf("(3) Salir\n");
+    printf("> ");
 
-    scanf("%d", &op);
+    if (scanf("%d", &op) != 1) {
+        printf("Entrada no válida.\n");
+        return 0;
+    }
 
-    switch(op) {
+    switch (op)
+    {
         case 1:
-            printf("Se seleccionó 'Ingresar'\n");
+            printf("Se seleccionó 'Ingresar'.\n");
             break;
         case 2:
-            printf("Se seleccionó 'Registrarse'\n");
+            printf("Se seleccionó 'Registrarse'.\n");
             break;
         case 3:
-            printf("Se seleccionó 'Salir'\n");
+            printf("Se seleccionó 'Salir'.\n");
             break;
         default:
-            printf("Opción no válida\n");
+            printf("Opción no válida.\n");
             break;
     }
 
