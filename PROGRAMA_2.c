cbe4ef32be838402b570a8ce891dac410608b5a6
DART1511 <ramirez.t.david510@gmail.com>
2025-10-09 14:07:39 -0600

    Create PROGRAMA_2.c

---
+++ PROGRAMA_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    if(0)
+    {
+    printf("Esta instruccion nunca se ejecuta\n");
+    printf("porque la condicion siempre es falsa (0).\n");
+    }
+    if (-38)
+    {
+        //
+        //
+        //
+        printf("Esta instruccion siempre se ejecuta.\n");
+    }
+
+    return 0;
+}
